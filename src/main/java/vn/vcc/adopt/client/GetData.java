add5bd5ce3ec81a8291e047e590a90a3ee752b2f
chiennv <chiennv97@gmail.com>
2017-08-14 10:52:21 +0700

    add throws

--- src/main/java/vn/vcc/adopt/client/GetData.java
+++ src/main/java/vn/vcc/adopt/client/GetData.java
@@ -9,83 +9,81 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class GetData {
-	// Timestamp cua file tren client
-	public static long timestamp = 0;
+    // Timestamp cua file tren client
+    public static long timestamp = 0;
 
-	/**
-	 * tra ve du lieu
-	 */
-	public static ReceivedObject getData(String url){
-		try {
-			HttpClient httpClient = new HttpClient();
-			httpClient.start();
-			//request type 1
-			long t1 = System.currentTimeMillis();
-			ContentResponse response = httpClient.GET(url + "?type=1");
-			System.out.println("Thoi gian request dau: "+(System.currentTimeMillis()-t1));
-			System.out.println(response.getContentAsString());
+    /**
+     * tra ve du lieu
+     */
+    public static ReceivedObject getData(String url) throws Exception {
 
-			//chuan bi cho request type 2
-			String[] strArr = response.getContentAsString().split("-");
-			timestamp = Long.parseLong(strArr[2]);
-			ReceivedObject receivedObject = new ReceivedObject(Integer.parseInt(strArr[1]), Integer.parseInt(strArr[0]), strArr[3]);
+        HttpClient httpClient = new HttpClient();
+        httpClient.start();
+        //request type 1
+        long t1 = System.currentTimeMillis();
+        ContentResponse response = httpClient.GET(url + "?type=1");
+        System.out.println("Thoi gian request dau: " + (System.currentTimeMillis() - t1));
+        System.out.println(response.getContentAsString());
 
-			//request type 2
-			ExecutorService es = Executors.newCachedThreadPool();
-			for(int i = 0;i< Integer.parseInt(strArr[0]);i++){
-				es.execute(new GetChunkThread(httpClient, receivedObject, i, url));
-			}
-			es.shutdown();
-			while(!es.awaitTermination(1, TimeUnit.MINUTES));
-			System.out.println("Thoi gian tinh xong het: "+(System.currentTimeMillis()-t1));
-			httpClient.stop();
-			return receivedObject;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
+        //chuan bi cho request type 2
+        String[] strArr = response.getContentAsString().split("-");
+        timestamp = Long.parseLong(strArr[2]);
+        ReceivedObject receivedObject = new ReceivedObject(Integer.parseInt(strArr[1]), Integer.parseInt(strArr[0]), strArr[3]);
 
-	/**
-	 * Tra ve timestamp cua file tren server
-	 * @return
-	 */
-	private static long getLastTime(String url){
-		long lastTime;
-		try {
-			HttpClient httpClient = new HttpClient();
-			httpClient.start();
-			ContentResponse response = httpClient.GET(url +"?type=3");
-			String lastTimeString = response.getContentAsString();
-			lastTime= Long.parseLong(lastTimeString);
-			httpClient.stop();
+        //request type 2
+        ExecutorService es = Executors.newCachedThreadPool();
+        for (int i = 0; i < Integer.parseInt(strArr[0]); i++) {
+            es.execute(new GetChunkThread(httpClient, receivedObject, i, url));
+        }
+        es.shutdown();
+        while (!es.awaitTermination(1, TimeUnit.MINUTES)) ;
+        System.out.println("Thoi gian tinh xong het: " + (System.currentTimeMillis() - t1));
+        httpClient.stop();
+        return receivedObject;
+    }
 
-			return lastTime;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return Long.parseLong(null);
-	}
-	/**
-	 * Check timestamp cua file o server (timestampServer) va timestamp cua file o client (timestapClient)
-	 * Neu khac nhau thi tra ve file ReceiveObject
-	 * Neu giong nhau thi tra ve null
-	 */
-	public static ReceivedObject getNewData(String url) {
-		long timestampServer = getLastTime(url);
-		long timestampClient = timestamp;
-		if(timestampServer != timestampClient){
-			return getData(url);
-		}
-		return null;
-	}
-	public static long getTimestamp() {
-		return timestamp;
-	}
+    /**
+     * Tra ve timestamp cua file tren server
+     *
+     * @return
+     */
+    private static long getLastTime(String url) {
+        long lastTime;
+        try {
+            HttpClient httpClient = new HttpClient();
+            httpClient.start();
+            ContentResponse response = httpClient.GET(url + "?type=3");
+            String lastTimeString = response.getContentAsString();
+            lastTime = Long.parseLong(lastTimeString);
+            httpClient.stop();
 
-	public static void setTimestamp(long timestamp) {
-		GetData.timestamp = timestamp;
-	}
+            return lastTime;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Long.parseLong(null);
+    }
+
+    /**
+     * Check timestamp cua file o server (timestampServer) va timestamp cua file o client (timestapClient)
+     * Neu khac nhau thi tra ve file ReceiveObject
+     * Neu giong nhau thi tra ve null
+     */
+    public static ReceivedObject getNewData(String url) throws Exception {
+        long timestampServer = getLastTime(url);
+        long timestampClient = timestamp;
+        if (timestampServer != timestampClient) {
+            return getData(url);
+        }
+        return null;
+    }
+
+    public static long getTimestamp() {
+        return timestamp;
+    }
+
+    public static void setTimestamp(long timestamp) {
+        GetData.timestamp = timestamp;
+    }
 
 }
